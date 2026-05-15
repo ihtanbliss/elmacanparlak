020af13cbb1cb3e2d1cece972cc7181ecf97d4b3
ihtanbliss <139270450+ihtanbliss@users.noreply.github.com>
2026-05-15 09:17:11 +0300

    Update ıhtan.py

--- ıhtan.py
+++ ıhtan.py
@@ -39,7 +39,7 @@ elif gender == "girl" or gender == "woman" or gender == "female":
          dress2 = input()
          if dress2 == "1" or dress2 == "2" or dress2 == "3" or dress2 == "4":
               print("You are all set for your journey! Go on and show the world what you got!")
-print("Loading.....\n\n\n""Please type 'goo' to continue")
+print("Loading.....\n\n\nPlease type 'goo' to continue")
 go = input()
 print("Your adventure begins in an enormous forest full of trees.\nTo survive you need resources, Type 'inv' to see your resources.")
 inv= input()
@@ -48,5 +48,24 @@ if inv == "inv":
     print("Rock: 5")
     print("Water: 2 Liters")
     print("A flint")
+    print("You have a lot to do. But everything begins with a fire and some sheen. Press '4' to light a fire.")
+    fire = input()
+    if fire == "4":
+        print("You lit up a smoldering fire! Now you don't feel as cold.\nYou recieved the achievement of fire.\nPress '1' to see your stats")
+        stats = input()
+        if stats == "1":
+            print("Craftmanship: Level 1\nOutdoormanship: Level 1(20% Progress)\n Strenght: Level 1\nStamina: Level 1\nArmory: Level 1")
+            print("The night begins to creep as you are rifting through the forest. You encounter a harmless looking bunny.\nWhat do you do?\nAttack, Talk, Walk past, Pet.")
+            action = input()
+            if action == "Attack":
+                print("The bunny was actually a hazardous monster. \nYou died.")
+            elif action == "Talk":
+                print("The bunny was actually was a subject of a spell. It talks back at you, uttering the words of mistery.\nYou gained 2 points of friendship.")
+            elif action == "Walk Past":
+                    print("Curiosity kills the cat. Nothing happened")
+            elif action == "Pet":
+                    print("You pet the cute bunny. It meows back at you. How? Nobody knows. You gained 1 points of cuteness.")
+
              
 
+    
